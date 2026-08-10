9dc91dcba0ee5c0017a40faffdddcb95b635b90d
Renanrp-05 <ren4nrodrigues05@gmail.com>
2026-08-10 10:00:16 -0300

    Usando o loop for para acessar o array

--- 10-loops-para-estruturas-de-dados.php
+++ 10-loops-para-estruturas-de-dados.php
@@ -13,6 +13,18 @@
     <div class="container">
         <h1>Loops para estruturas de dados</h1>
         <hr>
+
+        <?php
+            $meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"];
+        ?>
+
+        <h2>Usando o loop for para acessar o array</h2>
+
+        <ol>
+            <?php for($i = 0; $i < count($meses); $i++ ){ ?>
+                <li> <?= $meses[$i] ?> </li>
+            <?php } ?>
+        </ol>
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
