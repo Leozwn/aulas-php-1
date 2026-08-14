a576b12e7d7907b36da035535bebe897c47b005a
Renanrp-05 <ren4nrodrigues05@gmail.com>
2026-08-14 11:14:08 -0300

    Conversão de texto

--- 12-funcoes-nativas-para-textos.php
+++ 12-funcoes-nativas-para-textos.php
@@ -10,6 +10,39 @@
     <div class="container">
         <h1>Funções nativas para texto</h1>
         <hr>
+        <!-- mb -> multbyte: permite trabalhar com acentos, caracteres especiais, cedilha -->
+
+        <h2>mb_strlen()</h2>
+        <?php
+            $texto = "Uma frase qualquer, com acentos e cedilha: ação, ciência";
+        ?>
+        <p>String do exemplo: <?= $texto ?></p>
+        <p>Tamanho da string: <?= mb_strlen($texto) ?></p>
+
+        <h2>mb_strtoupper()</h2>
+        <p>Conversão para maiúsculas: <?= mb_strtoupper($texto) ?></p>
+
+        <h2>mb_strtolower()</h2>
+        <p>Conversão para minúsculas: <?= mb_strtolower($texto) ?></p>
+
+        <h2>str_replace() ou str_ireplace()</h2>
+        <?php $frase = "Esta é uma frase com palavras feias, como burro, idiota, chato demais e outras palavras ruins (bobo, panaca etc)! Chato mesmo! BOBO pra caramba. Também é um BOBÃO.";
+
+        // Procurando por UMA palavra e substituindo por outra
+        /* Se usar "str_ireplace" ele difencia palavras em maiusculo e as troca também como "BOBO" ele a troca por "cara legal". */
+        $fraseComSubstituicaoDePalavra = str_replace("bobo", "cara legal", $frase);
+
+        // Procurando por uma LISTA de palavras e substituindo por outra coisa
+        $fraseCensurada = str_ireplace(
+            ["panaca", "burro", "idiota", "chato", "bobão", "bobo", "BOBÃO"],
+            "🙈🙉🙊",
+            $frase
+        );
+        ?>
+
+        <p><mark>Frase original: <?= $frase ?></mark></p>
+        <p>Frase com substituição de palavra: <?= $fraseComSubstituicaoDePalavra ?></p>
+        <p>Frase censurada: <?= $fraseCensurada ?></p>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
