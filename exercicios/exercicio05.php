5cd34a5f23280eb07c173012102d5167faf3d273
Renanrp-05 <ren4nrodrigues05@gmail.com>
2026-08-14 09:18:30 -0300

    Termino do exercicio 5

--- exercicios/exercicio05.php
+++ exercicios/exercicio05.php
@@ -11,7 +11,30 @@
     <div class="container">
         <h1>Exercício 05: funções</h1>
 
-        
+        <?php
+            $nota1 = 7;
+            $nota2 = 8;
+            $nota3 = 5;
+            
+            function calcularMedia($nota1, $nota2, $nota3){
+                $total = ($nota1 + $nota2 + $nota3) / 3;
+                return $total;
+            }
+
+            $media = calcularMedia($nota1, $nota2, $nota3);
+            $formatarMedia = number_format($media, 2, ".");
+
+            function mostrarMedia(float $media):string{
+                if($media < 7) return "Reprovado";
+                return "Aprovado";
+            }
+        ?>
+
+        <hr>
+
+        <h3>Aluno: Marcos De Oliveira</h2>
+        <p>Média do Aluno: <b><?= $formatarMedia ?></b></p>
+        <p>Situação do aluno: <b><?= mostrarMedia($media) ?></b></p>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
