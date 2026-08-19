33f237165bda678e71650184aa8e6f9ca7896e7b
Leonardo <leonardo.0liveira03.00@gmail.com>
2026-08-19 10:30:36 -0300

    recursos somente um arquivo de php

---
+++ recursos.php
@@ -0,0 +1,7 @@
+<?php // recursos.php
+const ALUNO = "Tiago";
+$curso = "Técnico em Informática para Internet";
+$tecnologias = ["PHP", "SQL", "HTML", "CSS", "JavaScript"];
+function verificarIdade(int $idade):string {
+    return $idade >= 18 ? "maior" : "menor";
+}
