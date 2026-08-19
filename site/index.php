7f438687ff49717fbf8270a87a211be7731dd38f
Leonardo <leonardo.0liveira03.00@gmail.com>
2026-08-19 11:20:39 -0300

    criando um site com php

---
+++ site/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site usando PHP</title>
+</head>
+<body>
+    <head>
+        <h1>Site com PHP</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="cursos.php">Cursos</a>
+            <a href="duvidas.php">Dúvidas</a>
+        </nav>
+    </head>
+
+    <main>
+        <h2>Bem-vindo ao site de exemplo</h2>
+        <p>Esta é a primeira página do nosso site</p>
+    </main>
+</body>
+</html>
