b1db95bcbd330d9bf5eb22012d5a5ce8d0b02a36
Leonardo <leonardo.0liveira03.00@gmail.com>
2026-08-19 09:34:53 -0300

    Simulando ataque de injeção de código JS (XSS - CROSS site Scripting)

--- 15-filtros.php
+++ 15-filtros.php
@@ -58,6 +58,12 @@
         ?>
             <p>Nome informado: <?= $nomeCompletoSanitizado ?></p>
 
+        <?php 
+        // Simulando de um ataque de injeção de código JS (XSS - CROSS site Scripting)
+        $ataqueXSS ="<script>location = 'https://sp.senac.br'</script>";
+        ?>
+            <p>Teste: <?=filter_var($ataqueXSS, FILTER_SANITIZE_FULL_SPECIAL_CHARS ) ?></p>
+
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
