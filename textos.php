68bd2b46e0919a8df76504a89fb66c3244a5ae67
Leonardo <leonardo.0liveira03.00@gmail.com>
2026-08-19 11:02:26 -0300

    fazendo include entre os 3 arquivos

---
+++ textos.php
@@ -0,0 +1,17 @@
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Ex, quidem in nisi obcaecati maxime debitis perferendis, 
+asperiores assumenda sit ea necessitatibus cum.
+Fuga deleniti quas consequuntur, id, omnis ipsum vitae voluptatibus, 
+nihil doloremque reprehenderit pariatur voluptas vero tempore cupiditate blanditiis ratione! Voluptatibus obcaecati ullam doloribus deserunt, optio quia
+ culpa cupiditate?</p>
+
+ <?php include "lista.html"; ?>
+ 
+<p>Lorem ipsum dolor sit amet, consectetur 
+adipisicing elit. Facilis repellat repellendus animi eos,
+ distinctio ex sit nihil aliquam placeat sint?</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur 
+adipisicing elit. Facilis repellat repellendus animi eos,
+ distinctio ex sit nihil aliquam placeat sint?</p>
+
