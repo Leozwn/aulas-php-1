09d345317556a1a2a0c7bb61278f1387d3b3b9ec
Leonardo <leonardo.0liveira03.00@gmail.com>
2026-08-19 09:58:20 -0300

    feito filtro number float

--- 15-filtros.php
+++ 15-filtros.php
@@ -82,7 +82,25 @@ $ataqueEvitado = htmlspecialchars($ataqueXSS);
     ?>
         <p>Idade: <?= $idade ?></p>
 
-        
+    <h3>FILTER_SANITIZE_NUMBER_FLOAT</h3>    
+    <?php 
+    $precoInicial ="R$ 1000";
+    $desconto ="R$ 500.30";
+
+    $precoInicial = filter_var(
+        $precoInicial,FILTER_SANITIZE_NUMBER_FLOAT,FILTER_FLAG_ALLOW_FRACTION
+    );
+
+    $desconto = filter_var(
+        $desconto, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION
+    );
+
+    $precoFinal = $precoInicial - $desconto;
+    ?>    
+        <p>Preço Inicial: <?= $precoInicial ?></p>
+        <p>Desconto de: <?= $desconto ?></p>
+        <p>Preço Final: <?= $precoFinal ?></p>
+
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
