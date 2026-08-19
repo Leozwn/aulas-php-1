961773a738bb162dc96a4acd124d17eac1ad7ee5
Leonardo <leonardo.0liveira03.00@gmail.com>
2026-08-19 11:42:57 -0300

    criando arquivos(paginas do site) add conteudo dentro delas

---
+++ site/cursos.php
@@ -0,0 +1,13 @@
+<?php require "includes/cabecalho.php"?>
+
+    <h2>CURSOS</h2>
+    <p>Veja abaixo nossos cursos:</p>
+    <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>PYTHON</li>
+        <li>MYSQL</li>
+    </ul>
+
+<?php require "includes/rodape.php"?>
---
+++ site/duvidas.php
@@ -0,0 +1,14 @@
+<?php require "includes/cabecalho.php"?>
+
+    <h2>DÚVIDAS</h2>
+    <p>Perguntas e respostas frequentes:</p>
+    <details>
+        <summary>Quem sera  o proximo a ganha ?</summary>
+        <p>Leo</p>
+    </details>
+    <details>
+        <summary>Deus estara sempre com voce se voce crer nele</summary>
+        <p>Amém</p>
+    </details>
+
+<?php require "includes/rodape.php"?>
